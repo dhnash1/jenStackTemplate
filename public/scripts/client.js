0821637bb203e816795d837ef58e5a91d8d1bb53
T. Andrew Taton <thomas.andrew.taton@gmail.com>
2016-12-07 06:16:09 -0600

    New function displayArray in client.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -23,28 +23,29 @@ $(document).ready(function(){
       data: awardObjectToSend,
       success: function(response){
         // Displays array delivered by server as a table.
-        var arr = response.awardArray;
-        var tableText = '<table>\n<tr>\n<th class="event-column">Event</th>\n<th class="athlete-column">Athlete</th>\n<th class="award-column">Award</th>\n</tr>\n';
-        for (var i = 0; i < arr.length; i++) {
-          tableText += '<tr>\n<td>' + arr[i].eventName + '</td>\n';
-          tableText += '<td>' + arr[i].athleteName + '</td>\n';
-          tableText += '<td>' + arr[i].award + '</td>\n</tr>\n';
-        }
-        tableText += '</table>';
-        $('#awardList').html(tableText);
+        displayArray(response.awardArray);
       },
       error: function(){
         console.log( 'error with ajax call...');
       }
     });
-  }; // end getData
-
-  /// - buttons to test - ///
+  }; // end postData
 
   $( '#addAwardButton' ).on( 'click', function(){
+    // Button handler
     console.log( 'in addAwardButton on click' );
     postData();
+  }); // end addAwardButton handler
 
-  }); // end testGetButton
+  function displayArray(arr) {
+    var tableText = '<table>\n<tr>\n<th class="event-column">Event</th>\n<th class="athlete-column">Athlete</th>\n<th class="award-column">Award</th>\n</tr>\n';
+    for (var i = 0; i < arr.length; i++) {
+      tableText += '<tr>\n<td>' + arr[i].eventName + '</td>\n';
+      tableText += '<td>' + arr[i].athleteName + '</td>\n';
+      tableText += '<td>' + arr[i].award + '</td>\n</tr>\n';
+    }
+    tableText += '</table>';
+    $('#awardList').html(tableText);
+  } // end displayArray
 
 }); //end doc ready
